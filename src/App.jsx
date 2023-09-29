3b55ded83fed84ada0fdb9544186f56d352ad56e
RDSandakelum <dinushka.sandakelumr@gmail.com>
2023-09-29 21:32:46 +0530

    React hook form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import RegistrationForm from './Components/RegistrationForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-          </>
+      <RegistrationForm/>
+    </>
   )
 }
 
